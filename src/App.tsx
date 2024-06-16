7945de2b910526e75177f9c24d6838cf00ae8867
Ivan Pronin <pacernestit@rambler.ru>
2024-06-16 17:37:09 +0300

    dev feat (добавил базовый роутинг перемещения по основным страницам)

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+import { AuthPage } from "./pages/auth/auth";
 import { RegisterPage } from "./pages/register/register";
+import { RecPasswordPage } from "./pages/pass-rec/recpass";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <RegisterPage />
+      <Routes>
+        <Route path="/" element={<AuthPage />}></Route>
+        <Route path="/register" element={<RegisterPage />}></Route>
+        <Route path="/recpass" element={<RecPasswordPage />}></Route>
+      </Routes>
     </>
   );
 }
