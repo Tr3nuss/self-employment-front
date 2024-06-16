16b1ed5ccb296c1907557f705868b3b083273653
Ivan Pronin <pacernestit@rambler.ru>
2024-06-16 17:20:52 +0300

    dev feat (добавил разметку страницы восстановления пароля)

---
+++ src/pages/pass-rec/recpass.tsx
@@ -0,0 +1,63 @@
+import { FC } from "react";
+import "./passwordRecovery.css";
+import { Box, TextField, Button } from "@mui/material";
+
+export const RecPasswordPage: FC = () => {
+  const sendIntervalMessageSeconds: number = 120;
+  const sendIntervalToMailMessage: string = `${
+    (sendIntervalMessageSeconds / 60) % 60
+  }:00`;
+
+  return (
+    <>
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          minHeight: "100vh",
+        }}
+      >
+        <form className="recpass-input-form">
+          <h2 className="recpass-title">Забыли пароль?</h2>
+          <TextField
+            label="E-mail"
+            variant="standard"
+            sx={{ width: "500px" }}
+          ></TextField>
+          <Button
+            sx={{
+              width: "500px",
+              textTransform: "none",
+              bgcolor: "#1047A9",
+              color: "white",
+              fontSize: "24px",
+              fontWeight: "400",
+              borderRadius: "30px",
+            }}
+          >
+            Восстановить пароль
+          </Button>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              textAlign: "center",
+              gap: "30px",
+            }}
+          >
+            <p className="send-message-notification">
+              Мы вышлем вам сообщение на указанную почту
+            </p>
+            <p className="send-second-request">
+              Повторный запрос можно будет отправить через
+              {" " + sendIntervalToMailMessage} мин
+            </p>
+          </Box>
+
+          <p className="return-on-auth">Назад</p>
+        </form>
+      </Box>
+    </>
+  );
+};
