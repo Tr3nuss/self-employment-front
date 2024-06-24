7349c98bd2b9e3c587e8d2224c150a5112b5b447
Ivan Pronin <pacernestit@rambler.ru>
2024-06-24 15:13:37 +0300

    dev fix (сократил ProfilePage, улучшил читаемость)

--- src/components/profilePage/profile.tsx
+++ src/components/profilePage/profile.tsx
@@ -4,7 +4,7 @@ import telegramm from "../../images/profileImages/telegram.svg";
 import { useDispatch, useSelector } from "react-redux";
 import { setState } from "../../store/profileDataSlice/profileData";
 import { RootState } from "../../store/store";
-import { passportData } from "../../shared/utils/profilePassportData";
+import { ProfileData } from "./profileData";
 
 export const ProfilePage: FC = () => {
   const dispatch = useDispatch();
@@ -100,43 +100,7 @@ export const ProfilePage: FC = () => {
             Для ведения бухгалтерии в сервисе заполните следующие данные
           </p>
 
-          <Box>
-            {profileData && (
-              <>
-                <Box sx={{ display: "flex", gap: "20px", margin: "40px 0px" }}>
-                  <TextField
-                    label="ИНН"
-                    variant="standard"
-                    sx={{ width: 243 }}
-                  />
-                  <TextField
-                    label="Адрес регистрации"
-                    variant="standard"
-                    sx={{ width: 506 }}
-                  />
-                </Box>
-
-                <p style={{ fontSize: 24, margin: "72px 0px 40px 0px" }}>
-                  Паспортные данные
-                </p>
-
-                <Box sx={{ display: "flex", gap: "20px" }}>
-                  {passportData.map((item, index) => (
-                    <TextField
-                      sx={{ width: item.width }}
-                      key={index}
-                      variant="standard"
-                      label={item.label}
-                    />
-                  ))}
-                </Box>
-
-                <p style={{ fontSize: 24, marginTop: "70px" }}>
-                  Платежные реквизиты
-                </p>
-              </>
-            )}
-          </Box>
+          <Box>{profileData && <ProfileData />}</Box>
         </Box>
       </Box>
     </>
---
+++ src/components/profilePage/profileData.tsx
@@ -0,0 +1,37 @@
+import { FC } from "react";
+import { Box, TextField } from "@mui/material";
+import { passportData } from "../../shared/utils/profilePassportData";
+
+export const ProfileData: FC = () => {
+  return (
+    <>
+      <Box>
+        <Box sx={{ display: "flex", gap: "20px", margin: "40px 0px" }}>
+          <TextField label="ИНН" variant="standard" sx={{ width: 243 }} />
+          <TextField
+            label="Адрес регистрации"
+            variant="standard"
+            sx={{ width: 506 }}
+          />
+        </Box>
+
+        <p style={{ fontSize: 24, margin: "72px 0px 40px 0px" }}>
+          Паспортные данные
+        </p>
+
+        <Box sx={{ display: "flex", gap: "20px" }}>
+          {passportData.map((item, index) => (
+            <TextField
+              sx={{ width: item.width }}
+              key={index}
+              variant="standard"
+              label={item.label}
+            />
+          ))}
+        </Box>
+
+        <p style={{ fontSize: 24, marginTop: "70px" }}>Платежные реквизиты</p>
+      </Box>
+    </>
+  );
+};
