0bc69bcde1baa09a8718de29c6b98da24c7aced7
Ivan Pronin <pacernestit@rambler.ru>
2024-06-25 18:56:47 +0300

    dev feat (добавил разметку для Customers)

--- src/components/customerPage/customers.tsx
+++ src/components/customerPage/customers.tsx
@@ -1,12 +1,56 @@
 import { NavLink } from "react-router-dom";
 import { Box } from "@mui/material";
+import { blue } from "@mui/material/colors";
 
 export const CustomersPage = () => {
   return (
     <>
       <Box sx={{ maxWidth: "1740px" }}>
-        <Box sx={{ maxWidth: "1460px", margin: "40px auto" }}>
-            
+        <Box sx={{ maxWidth: "1460px", margin: "20px auto" }}>
+          <p>Главная / Заказчики</p>
+
+          <Box>
+            <NavLink
+              to="*"
+              style={({ isActive }) => ({
+                textDecoration: isActive ? "underline dotted blue" : "none",
+              })}
+            >
+              TAB
+            </NavLink>
+            <NavLink
+              to="*"
+              style={({ isActive }) => ({
+                textDecoration: isActive ? "underline dotted blue" : "none",
+              })}
+            >
+              TAB
+            </NavLink>
+            <NavLink
+              to="*"
+              style={({ isActive }) => ({
+                textDecoration: isActive ? "underline dotted blue" : "none",
+              })}
+            >
+              TAB
+            </NavLink>
+            <NavLink
+              to="*"
+              style={({ isActive }) => ({
+                textDecoration: isActive ? "underline dotted blue" : "none",
+              })}
+            >
+              TAB
+            </NavLink>
+            <NavLink
+              to="*"
+              style={({ isActive }) => ({
+                textDecoration: isActive ? "underline dotted blue" : "none",
+              })}
+            >
+              TAB
+            </NavLink>
+          </Box>
         </Box>
       </Box>
     </>
